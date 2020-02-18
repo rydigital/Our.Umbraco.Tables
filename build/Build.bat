5b7cc72a10b17d565b9406a59308be8d2e192d54
Jack <jackstunell@gmail.com>
2020-02-18 14:41:17 +0000

    feat: nuget + umbraco building works

--- build/Build.bat
+++ build/Build.bat
@@ -1,4 +1,2 @@
-xcopy ..\src\Our.Umbraco.TableGenerator\App_Plugins\Our.Umbraco.TableGenerator  ..\samples\Our.Umbraco.TableGenerator.Demo\App_Plugins /e /i /h /s /y
-
 Call nuget.exe restore ..\src\Our.Umbraco.TableGenerator.sln
 Call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" package.build.xml /bl /p:Configuration=Release
