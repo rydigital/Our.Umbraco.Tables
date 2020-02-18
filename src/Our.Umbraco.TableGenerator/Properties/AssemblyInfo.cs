ff0d1b24cbdaa4877c2e37df6116858b9b59c86b
Jack <jackstunell@gmail.com>
2020-02-18 14:42:15 +0000

    chore: moved App_Plugins to UI folder for packaging tools

--- src/Our.Umbraco.TableGenerator/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.TableGenerator/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Radley Yeldar")]
 [assembly: AssemblyProduct("Our.Umbraco.TableGenerator")]
-[assembly: AssemblyCopyright("Copyright © Radley Yeldar 2020")]
+[assembly: AssemblyCopyright("Copyright ? Radley Yeldar 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+// [assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
